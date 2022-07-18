764518570278cf29f09c59ab4bad60a4735cb720
JosephGlz <joseph220199@gmail.com>
2022-07-17 19:11:49 -0500

    add dynamic GET route

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const app = express();
                   users: mockUserData
              })
         })
+        app.get('/users/:id',function(req,res){
+            console.log(req.params.id)
+            res.json({
+                success: true,
+                message: 'got one user',
+                user: req.params.id
+            })
+        })
         
 app.listen(8000,function(){
     console.log("server is running")
